c271b06b0d3eb789a100c4102a5209972bfc9c8d
hellh4mmer <draimaan@moh.gov.my>
2025-05-22 15:44:26 +0800

    fix: use logger

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,5 +1,7 @@
-const errorHandler = (err, req, res, next) => {
-	console.error(err.stack);
+import logger from "../utils/logger";
+
+const errorHandler = (err, _req, res, _next) => {
+	logger.error(err.stack);
 	res.status(500).json({ message: "Internal Server Error" });
 };
 
