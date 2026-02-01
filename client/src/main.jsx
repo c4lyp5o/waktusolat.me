68a06f5833beddecdedcd6dfab9cdc92c75f8919
c4lyp5o <calypso@calypsocloud.one>
2026-02-01 12:43:14 +0800

    fix: add tailwind to main css

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { ErrorBoundary } from "./components/ErrorBoundary";
 
-import "./styles/pico.min.css";
+import "./main.css";
 
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
-		<main className="container">
+		<ErrorBoundary>
 			<App />
-		</main>
+		</ErrorBoundary>
 	</StrictMode>,
 );
